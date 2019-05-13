dbcda10ee9c428010438da7dd94642dbd2cc2af0
jass <ghaithix@hotmail.com>
2019-05-13 20:25:32 +0300

    average of stick hough lines slope and intercept and coordinates conversion

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 def nothing(x):
     pass
 
+# cv2.namedWindow('Canny_settings')
+# cv2.createTrackbar('threshold1','Canny_settings',1,249,nothing)
+# cv2.createTrackbar('threshold2','Canny_settings',1,249,nothing)
 
-cv2.namedWindow('Canny_settings')
-cv2.createTrackbar('threshold1','Canny_settings',1,249,nothing)
-cv2.createTrackbar('threshold2','Canny_settings',1,249,nothing)
 def canny(image, threshold1 = 33,threshold2 = 0) :
   blur = cv2.GaussianBlur(image, (5,5), 0)
   canny = cv2.Canny(blur.copy(), threshold1, threshold2)
@@ -62,6 +62,7 @@ def stickLineDetection(canny_image, original_image):
       x1, y1, x2, y2 = line[0]
       cv2.line(original_image, (x1, y1), (x2, y2), (0, 255, 0), 5)
   cv2.imshow("HoughLines", original_image)
+  return lines
 
 
 # cv2.namedWindow('morph_settings')
@@ -75,23 +76,72 @@ def morph(image):
   #morph = cv2.dilate(morph, kernel, iterations=1)
   #cv2.imshow("morph", morph)
   pass
+
+def make_coordinates(image, fit_lines_average):
+  slope, intercept = fit_lines_average
+
+  y1 = image.shape[0]
+  y2 = int(y1*(4/5))
+  x1 = int((y1 - intercept)/slope)
+  x2 = int((y2 - intercept)/slope)
+  return np.array([x1,y1,x2,y2])
+
+
+def average_slope_intercept(image , lines):
+  fitLines = [] 
+
+  for line in lines:
+    x1, y1, x2, y2 = line.reshape(4)
+    parameters = np.polyfit((x1,x2), (y1,y2), 1)
+    slope = parameters[0]
+    intercept = parameters[1]
+    fitLines.append((slope, intercept))
+    
+  fit_lines_average = np.average(fitLines, axis = 0)
   
+  return make_coordinates(image, fit_lines_average)
+
+def display_stick(image, line):
+  stickLine_image = np.zeros_like(image)
+  if line is not None:
+    x1, y1, x2, y2 = line.reshape(4)
+    print('x1: %d, y1:%d, x2:%d, y2:%d'%(x1, y1, x2, y2) )
+    cv2.line(stickLine_image, (x1,y1), (x2,y2), (255,0,0), 5)     
+  return stickLine_image
+
+
 image = cv2.imread('img.jpg')
+plt.imshow(image)
+plt.show()
 
 while (1):
 
   threshold1 = cv2.getTrackbarPos('threshold1','Canny_settings')
   threshold2 = cv2.getTrackbarPos('threshold2','Canny_settings')
   board = image.copy()
+  #1
   stickColorIsolate_img = stickColorIsolate(board)
+  #cv2.imshow("Stick_Image>Color Iso", stickColorIsolate_img)
+  
   #morph(stickColorIsolate_img)
+  
+  #2
   canny_image = canny(stickColorIsolate_img, 250, 250)# threshold1= threshold1, threshold2 = threshold2)
-  houghStick = stickLineDetection(canny_image, board)
+  #cv2.imshow("Canny_Image", canny_image)
+  
+  #3
+  lines = stickLineDetection(canny_image, board)
+  line = average_slope_intercept(board, lines)
+  stick_average = display_stick(board, line)
+  averaged_combined_image = cv2.addWeighted(board, 0.8, stick_average, 1, 1)
+  
+  cv2.imshow("Averaged Hough", averaged_combined_image)
 
-  cv2.imshow("Stick_Image>Color Iso", stickColorIsolate_img)
-  cv2.imshow("Canny_Image", canny_image)
   #cv2.imshow("Color Iso>Hough", houghStick)
   if cv2.waitKey(100) == 13:
     break
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
+
+
+
